4a7bc097c079d20d7fdb337433128fe377043ecb
d0rj <hanaan@internet.ru>
2021-01-22 19:53:05 +0300

    Added multiple variable input by console support; updated Readme due to this.

--- main.py
+++ main.py
@@ -14,13 +14,20 @@ def load_template(path: str) -> List[str]:
 
 def main() -> None:
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-X', '--X', type=str, default='коммунизм', metavar='X', help='Значение переменной слова X.')
-	parser.add_argument('-f', '--file', type=pathlib.Path, default='./templates/zvonov.txt', metavar='file', help='Входной файл с шаблоном.')
+	parser.add_argument('-X', '--X', type=str, default='коммунизм', metavar='X', 
+		help='Значение переменной слова X.')
+	parser.add_argument('-f', '--file', type=pathlib.Path, default='./templates/zvonov.txt', metavar='file', 
+		help='Входной файл с шаблоном.')
+	parser.add_argument('-v', '--variable', action='append', type=lambda kv: kv.split("="), dest='variables', 
+		help='Добавляет в список переменных значение. Может применяться несколько раз. Аргумент должен выглядеть как X=слово, где X - любое название вашей переменной')
 
 	args = parser.parse_args()
 
-	engine = TemplateEngine({'X': args.X, 'Y': 'монархизм'})
-	print(engine.process_template(''.join(load_template(args.file))))
+	filename = args.file
+	variables = dict(args.variables)
+
+	engine = TemplateEngine(variables)
+	print(engine.process_template(''.join(load_template(filename))))
 
 
 if __name__ == '__main__':

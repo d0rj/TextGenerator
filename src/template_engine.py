1a8734c259510fa53784fa572a26db0b1a5ccb55
d0rj <hanaan@internet.ru>
2021-01-23 13:21:03 +0300

    Template engine optimizations: 21% speedup.
    Quantitatively, this difference is between 756 µs and 592 µs for test template.

--- src/template_engine.py
+++ src/template_engine.py
@@ -3,57 +3,39 @@ from typing import Dict, Literal
 from src.word_morpher import WordMorpher
 
 
-class TemplateEngine:
-	ANY_VAR_REGEX = r'([A-Z_-]+)'
-	
+class TemplateEngine:	
 	variables: Dict[str, str] = {}
 	ignore_unknown = False
 	
 	__morpher = WordMorpher()
-	__vars_regexes: Dict[str, str] = {}
-	__general_regex: Literal = ''
+	__general_regex: Literal = r'\[[\ ]*([A-Z_-]+)[\ ]*[|]{0,1}[\ ]*(\w*)[\ ]*\]'
 
 	
 	def __init__(self, variables: Dict[str, str], ignore_unknown: bool = False) -> None:
 		self.variables = { k.upper() : v for k, v in variables.items() }
 		self.ignore_unknown = ignore_unknown
 
-		self.__general_regex = self.__var_regex(self.ANY_VAR_REGEX)
-
-		for var in variables.keys():
-			self.__vars_regexes[var] = self.__var_regex(var)
-
 
 	def process_template(self, template: str) -> str:
 		result = template
-		
-		if self.ignore_unknown:
-			for var in self.variables:
-				result = self.process_variable(result, var, self.variables[var])			
-		else:
-			parsed_vars = [m.group(1) for m in re.finditer(self.__general_regex, template)]
 
-			for parsed_var in parsed_vars:
-				if parsed_var in self.variables.keys():
-					result = self.process_variable(result, parsed_var, self.variables[parsed_var])
+		parsed = [m for m in re.finditer(self.__general_regex, template)]
+		parsed = [(p.group(), p.group(1), p.group(2)) for p in parsed]
+
+		matches = [p[0] for p in parsed]
+		parsed_vars = [p[1] for p in parsed]
+		forms = [p[2] for p in parsed]
+
+		for index, match in enumerate(matches):
+
+			if parsed_vars[index] in self.variables:
+				processed_word = self.__morpher.process_word(self.variables[parsed_vars[index]], forms[index])
+				result = result.replace(match, processed_word, 1)
+			else:
+				if self.ignore_unknown:
+					continue
 				else:
-					raise Exception('No such variable: ' + parsed_var + '.')
+					raise Exception('No such variable: ' + parsed_vars[index] + '.')
 
-		return result
-
-
-	def process_variable(self, template: str, var_name: str, var_value: str) -> str:
-		parsed = [m for m in re.finditer(self.__vars_regexes[var_name], template)]
-
-		matches = [p.group() for p in parsed]
-		forms = [p.group(1) for p in parsed]
 		
-		result = template
-		for index, match in enumerate(matches, start=0):
-			result = result.replace(match, self.__morpher.process_word(var_value, forms[index]))
-
 		return result
-
-
-	def __var_regex(self, var_name: str) -> str:
-		return r'\[[\ ]*' + var_name + r'[\ ]*[|]{0,1}[\ ]*(\w*)[\ ]*\]'
